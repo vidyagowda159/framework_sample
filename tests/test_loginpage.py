a64287ed5d077931b1590ed3537e724aa4affa9a
vidyagowda159 <vidyagowda005@gmail.com>
2022-12-15 14:53:47 +0530

    modified yaml file

--- tests/test_loginpage.py
+++ tests/test_loginpage.py
@@ -3,17 +3,11 @@ from src.login_page import LoginPage
 
 class TestLoginPage:
 
-    def test_valid_credential(self, init_driver):
-        driver = init_driver
-        try:
-            lp = LoginPage()
-            lp.launch_url()
-            lp.enter_username("admin")
-            lp.enter_password("manager")
-            lp.select_checkbox()
-            lp.click_login()
-
-        except Exception as msg:
-            driver.save_screenshot("screenshot1.png")
-            raise msg
+    def test_valid_credential(self):
+        lp = LoginPage()
+        lp.launch_url()
+        lp.enter_username("admin")
+        lp.enter_password("manager")
+        lp.select_checkbox()
+        lp.click_login()
 
